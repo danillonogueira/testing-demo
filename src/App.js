a1f849b8ef2d17be99925cff0996661ae4cccf71
Danillo Nogueira <dnll_nog@yahoo.com>
2021-08-11 18:12:15 -0300

    naming refactor

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ import Button from './components/Button';
 import React, { useState } from 'react';
 // import History from './components/History';
 
-function getRandomInt(min, max) {
-  const _min = Math.ceil(min);
-  const _max = Math.floor(max);
-
-  return Math.floor(Math.random() * (_max - _min)) + _min;
-}
-
 const App = () => {
-  const [ dice1, setDice1 ] = useState(1);
-  const [ dice2, setDice2 ] = useState(2);
+  const [ firstDice, setFirstDice ] = useState(6);
+  const [ secondDice, setSecondDice ] = useState(3);
+
+  const getRandomInt = (min, max) => {
+    const _min = Math.ceil(min);
+    const _max = Math.floor(max);
+
+    return Math.floor(Math.random() * (_max - _min)) + _min;
+  };
 
   const roll = () => {
-    setDice1(getRandomInt(1, 7));
-    setDice2(getRandomInt(1, 7));
+    setFirstDice(getRandomInt(1, 7));
+    setSecondDice(getRandomInt(1, 7));
   };
 
   return (
     <Container>
       <GlobalStyles />
-      <Dice>{dice1}</Dice>
-      <Dice>{dice2}</Dice>
+      <Dice>{firstDice}</Dice>
+      <Dice>{secondDice}</Dice>
       <Button clickHandler={roll}>Roll</Button>
     </Container>
   );
